fbc5b184b7bc00e61ec4e2503ce01a786bbe2312
jlchang <jlchang@broadinstitute.org>
2017-12-05 09:44:49 -0500

    updated name to reflect miseq-specific nature of script

---
+++ scripts/ChIPseq/paired/v0.03/createMiseqInputFile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#####
+#
+# createMiseqInputFile.sh <path> 
+# 
+# given path (that includes Data/Intensities/BaseCalls),
+#     list samplename and fastq paths
+#
+#####
+
+
+# Any subsequent commands which fail will cause the shell script to exit immediately
+set -e
+
+dirPath=$1
+
+if [  ! -d "$dirPath" ]
+  then
+    echo "Unable to find $dirPath, please check the provided path"
+    exit
+fi
+
+
+ls -1 $1/*_R1_* | grep -v Undetermined > R1
+ls -1 $1/*_R2_* | grep -v Undetermined > R2
+for i in $(cat R1); do basename $i | cut -d "_" -f 1-2 >> sample;  done
+paste sample R1 R2 > input_data.tsv
+rm sample R1 R2
+echo "input_data.tsv created"
